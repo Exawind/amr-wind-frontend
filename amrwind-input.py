2cc4bacc0f2971072e73663b322d299bb509fc5e
Lawrence Cheung <lcheung@sandia.gov>
2021-03-25 18:00:48 -0600

    Updated plotting

--- amrwind-input.py
+++ amrwind-input.py
@@ -10,14 +10,93 @@ if sys.version_info[0] < 3:
 else:
     import tkinter as Tk
 
+import numpy as np
+from matplotlib.collections import PatchCollection
+from matplotlib.patches     import Rectangle
+
 class MyApp(tkyg.App, object):
     def __init__(self, *args, **kwargs):
         super(MyApp, self).__init__(*args, **kwargs)
-        t   = np.arange(0, 3, .01)
         self.fig.clf()
-        self.fig.add_subplot(111).plot(t, t**2)
+        #t   = np.arange(0, 3, .01)
+        #self.fig.add_subplot(111).plot(t, t**2)
+        self.fig.text(0.35,0.5,'Welcome to\nAMR-Wind')
 
         self.formatgridrows()
+
+    def plotDomain(self):
+        # Clear and resize figure
+        canvaswidget=self.figcanvas.get_tk_widget()
+        w,h1 = self.winfo_width(), self.winfo_height()
+        canvaswidget.configure(width=w-self.leftframew-10, height=h1-75)
+        self.fig.clf()
+        ax=self.fig.add_subplot(111)
+        ax.clear()
+        
+        # Get the variables
+        corner1  = self.inputvars['prob_lo'].getval()
+        corner2  = self.inputvars['prob_hi'].getval()
+        xychoice = self.inputvars['plot_chooseview'].getval()
+        if xychoice == 'XY':
+            ix,iy = 0,1
+            xstr, ystr='x','y'
+        elif xychoice == 'XZ':
+            ix,iy = 0,2
+            xstr, ystr='x','z'
+        elif xychoice == 'YZ':
+            ix,iy = 1,2
+            xstr, ystr='y','z'
+        # Wind direction
+        windvec  = self.inputvars['ABL_velocity'].getval()
+        windh    = self.inputvars['forcing_height'].getval()
+        # North direction
+        northdir = self.inputvars['north_vector'].getval()
+
+        # Do the actual plot here
+        x1 = corner1[ix]
+        y1 = corner1[iy]
+        x2 = corner2[ix]
+        y2 = corner2[iy]
+        Lx = x2-x1
+        Ly = y2-y1
+        Cx = 0.5*(x1+x2)
+        Cy = 0.5*(y1+y2)
+        rect=Rectangle((x1, y1), Lx, Ly, color='gray', alpha=0.25)
+        ax.add_patch(rect)
+        ax.set_xlim([Cx-Lx*0.55, Cx+Lx*0.55])
+        ax.set_ylim([Cy-Ly*0.55, Cy+Ly*0.55])
+
+        # Plot the wind vector
+        arrowlength = 0.1*np.linalg.norm([Lx, Ly])
+        plotwindvec = np.array(windvec)
+        plotwindvec = plotwindvec/np.linalg.norm(plotwindvec)*arrowlength
+        windcenter = [Cx, Cy, windh]
+        if np.linalg.norm([plotwindvec[ix], plotwindvec[iy]])>0.0:
+            ax.arrow(windcenter[ix], windcenter[iy], 
+                     plotwindvec[ix], plotwindvec[iy], 
+                     width=0.05*arrowlength)
+        
+        # Plot the north arrow
+        northlength = 0.1*np.linalg.norm([Lx, Ly])
+        plotnorthvec  = np.array(northdir)
+        plotnorthvec  = plotnorthvec/np.linalg.norm(plotnorthvec)*northlength
+        compasscenter = [Cx-0.4*Lx, Cy+0.35*Ly, windh]
+        
+        if np.linalg.norm([plotnorthvec[ix], plotnorthvec[iy]])>0.0:
+            ax.arrow(compasscenter[ix], compasscenter[iy], 
+                     plotnorthvec[ix], plotnorthvec[iy], 
+                     color='r', head_width=0.1*northlength, linewidth=0.5)
+            ax.text(compasscenter[ix], compasscenter[iy], 
+                    'N', color='r', ha='right', va='top')
+        
+        ax.set_aspect('equal')
+        ax.set_xlabel('%s [m]'%xstr)
+        ax.set_ylabel('%s [m]'%ystr)
+        ax.set_title(r'Domain')
+        self.figcanvas.draw()
+        #self.figcanvas.show()
+
+        return
 
 if __name__ == "__main__":
     title='AMR-Wind input creator'
