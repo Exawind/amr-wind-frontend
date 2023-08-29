e13221bc09170cf03723ac54f0c34463a5b0d3ec
Lawrence Cheung <15526007+lawrenceccheung@users.noreply.github.com>
2023-08-29 14:58:43 -0600

    Added postproamrwindsample_xarray.py

---
+++ postproamrwindsample_xarray.py
@@ -0,0 +1,41 @@
+# Load the libraries
+import postproamrwindsample as ppsample
+import numpy             as np
+
+import xarray as xr
+
+extractvar = lambda xrds, var, i : xrds[var][i,:].data.reshape(tuple(xrds.attrs['ijk_dims'][::-1]))
+
+def getPlaneXR(ncfile, itimevec, varnames, vxvar='velocityx',
+               vyvar='velocityy', vzvar='velocityz', groupname=None,
+               verbose=0):
+    # Create a fresh db dictionary
+    db = {}
+    for v in varnames: db[v] = {}
+    db['timesteps'] = []
+    # Now load the ncfile data
+    if groupname is None:
+        groups= ppsample.getGroups(ppsample.loadDataset(ncfile))
+        group = groups[0]
+    else:
+        group = groupname
+    with xr.open_dataset(ncfile, group=group) as ds:
+        xm = ds['coordinates'].data[:,0].reshape(tuple(ds.attrs['ijk_dims'][::-1]))
+        ym = ds['coordinates'].data[:,1].reshape(tuple(ds.attrs['ijk_dims'][::-1]))
+        zm = ds['coordinates'].data[:,2].reshape(tuple(ds.attrs['ijk_dims'][::-1]))
+        dtime=xr.open_dataset(ncfile)
+        dtime.close()
+        db['x'] = xm
+        db['y'] = ym
+        db['z'] = zm        
+        for itime in itimevec:
+            if verbose>0:
+                print("extracting iter "+repr(itime))
+            db['timesteps'].append(itime)
+            for v in varnames:
+                vvar = extractvar(ds, v, itime)
+                db[v][itime] = vvar
+            vx = extractvar(ds, vxvar, itime)
+            vy = extractvar(ds, vyvar, itime)
+            vz = extractvar(ds, vzvar, itime)
+    return db
