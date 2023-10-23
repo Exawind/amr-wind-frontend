4913e07f2a64f94bfdd25d91f53b49ccdd11cd1f
Lawrence Cheung <15526007+lawrenceccheung@users.noreply.github.com>
2023-10-23 10:04:22 -0600

    Update postproamrwindsample_xarray.py with getLineXR

--- postproamrwindsample_xarray.py
+++ postproamrwindsample_xarray.py
@@ -43,3 +43,48 @@ def getPlaneXR(ncfile, itimevec, varnames, vxvar='velocityx',
             for k, g in ds.attrs.items():
                 db[k] = g
     return db
+
+def getLineXR(ncfile, itimevec, varnames, vxvar='velocityx',
+              vyvar='velocityy', vzvar='velocityz', groupname=None,
+              verbose=0, includeattr=False, gettimes=False):
+    # Create a fresh db dictionary
+    db = {}
+    for v in varnames: db[v] = {}
+    db['timesteps'] = []
+    timevec = None
+    if gettimes:
+        timevec = ppsample.getVar(ppsample.loadDataset(ncfile), 'time')
+        db['times'] = []
+    # Now load the ncfile data
+    if groupname is None:
+        groups= ppsample.getGroups(ppsample.loadDataset(ncfile))
+        group = groups[0]
+    else:
+        group = groupname
+    with xr.open_dataset(ncfile, group=group) as ds:
+        #reshapeijk = ds.attrs['ijk_dims'][::-1]
+        xm = ds['coordinates'].data[:,0] #.reshape(tuple(reshapeijk))
+        ym = ds['coordinates'].data[:,1] #.reshape(tuple(reshapeijk))
+        zm = ds['coordinates'].data[:,2] #.reshape(tuple(reshapeijk))
+        dtime=xr.open_dataset(ncfile)
+        dtime.close()
+        db['x'] = xm
+        db['y'] = ym
+        db['z'] = zm
+        for itime in itimevec:
+            if verbose>0:
+                print("extracting iter "+repr(itime))
+            db['timesteps'].append(itime)
+            if gettimes:
+                db['times'].append(float(timevec[itime]))
+            for v in varnames:
+                vvar = ds[v].data[itime,:]
+                #vvar = extractvar(ds, v, itime)
+                db[v][itime] = vvar
+            #vx = extractvar(ds, vxvar, itime)
+            #vy = extractvar(ds, vyvar, itime)
+            #vz = extractvar(ds, vzvar, itime)
+        if includeattr:
+            for k, g in ds.attrs.items():
+                db[k] = g
+    return db
