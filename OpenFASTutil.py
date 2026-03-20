61328b37cd685d71d80a1047c5805ac17dbe6af6
Mike Sprague <michaelasprague@gmail.com>
2026-03-20 13:08:32 -0600

    Update copyright holder in OpenFASTutil.py

--- OpenFASTutil.py
+++ OpenFASTutil.py
@@ -2,7 +2,7 @@
 # PYTHON_ARGCOMPLETE_OK
 
 #
-# Copyright (c) 2022, Alliance for Sustainable Energy
+# Copyright (c) 2022, Alliance for Energy Innovation
 #
 # This software is released under the BSD 3-clause license. See LICENSE file
 # for more details.
